205d50452da132d5c9325102a47733c69f076da6
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2024-05-23 15:40:41 +0530

    task3

---
+++ Task3/books/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using Books.Models;
+using Books.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace books.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookService _bookService;
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("GetBooks")]
+        public ActionResult<List<Book>> Get() => _bookService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> Get(int id)
+        {
+            var book = _bookService.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+        [HttpPost("{id}")]
+        public ActionResult<Book> Post(Book book)
+        {
+            _bookService.Add(book);
+            return CreatedAtAction(nameof(Get), new { book.id },book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Book book)
+        {
+            if (id != book.id)
+            {
+                return BadRequest();
+            }
+            var existingBook = _bookService.GetById(id);
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+            _bookService.Update(book);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = _bookService.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _bookService.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ Task3/books/Services/BookServices.cs
@@ -0,0 +1,38 @@
+﻿using Books.Models;
+
+namespace Books.Services
+{
+    public class BookService
+    {
+        private readonly List<Book> _books;
+        public BookService()
+        {
+            _books = new List<Book>
+            {
+                new Book{ id=1, Title="1984", Author="George", Genre="dystopian"}
+            };
+        }
+        public List<Book> GetAll() 
+        { 
+            return _books;
+        }
+        public Book GetById(int id) => _books.FirstOrDefault(b => b.id == id);
+        public void Add(Book book) => _books.Add(book);
+        public void Update(Book book)
+        {
+            var index = _books.FindIndex(b => b.id == book.id);
+            if (index != -1)
+            {
+                _books[index] = book;
+            }
+        }
+        public void Delete(int id) // Implement this method
+        {
+            var book = GetById(id);
+            if (book != null)
+            {
+                _books.Remove(book);
+            }
+        }
+    }
+}

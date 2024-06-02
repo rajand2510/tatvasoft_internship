4feee8b544673d337c94d0c60b2c7ad98289a8c8
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2024-06-02 20:39:02 +0530

    task 6

--- Task3/books/obj/Debug/net8.0/books.AssemblyInfo.cs
+++ Task3/books/obj/Debug/net8.0/books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2878014c35188892a931ed9d89a5657202c57379")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e610ee76901f4de42ae4b61df07fd1627dca170e")]
 [assembly: System.Reflection.AssemblyProductAttribute("books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
